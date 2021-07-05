43bfb4310e469126516e425f443a780f8f4dc174
CaTxDog <57478694+CaTxDog@users.noreply.github.com>
2021-07-05 22:48:36 +0600

    Изменение форматирования

--- sandbox/src/main/java/ru/truakdsg/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/truakdsg/pft/sandbox/MFP.java
@@ -1,8 +1,8 @@
 package ru.truakdsg.pft.sandbox;
 
 public class MFP {
-	
-	public static void main (String[] args) {
-		System.out.println("Hello, world!");
-	}
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 }
