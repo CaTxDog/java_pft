85116b6d0c3e5b077691edba5f8dd56c70abdea9
CaTxDog <57478694+CaTxDog@users.noreply.github.com>
2021-07-06 18:28:53 +0600

    ДЗ №2.4
    Переработана функция в метод

--- sandbox/src/main/java/ru/truakdsg/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/truakdsg/pft/sandbox/MFP.java
@@ -2,13 +2,10 @@ package ru.truakdsg.pft.sandbox;
 
 public class MFP {
 
-  public static double distance(Point p1, Point p2)
-  {
-    return Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
-  }
+
   public static void main(String[] args) {
     Point p1 = new Point(1,2);
     Point p2 = new Point(3,4);
-    System.out.println(distance(p1,p2));
+    System.out.println("Расстояние между точками P1("+p1.x+","+p1.y+") и P2("+p2.x+","+p2.y+") = " + p1.distance(p2));
   }
 }
--- sandbox/src/main/java/ru/truakdsg/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/truakdsg/pft/sandbox/Point.java
@@ -1,12 +1,17 @@
 package ru.truakdsg.pft.sandbox;
 
 public class Point {
-  public double x;
-  public double y;
+  double x;
+  double y;
 
-  public Point(double x, double y) {
+  public Point(double x1, double y1) {
 
-    this.x = x;
-    this.y = y;
+    this.x = x1;
+    this.y = y1;
+  }
+  //Обращаемся к отрибутам выше
+  public double distance(Point p2)
+  {
+    return Math.sqrt(Math.pow((p2.x-this.x),2)+Math.pow((p2.y-this.y),2));
   }
 }
