c771eb5e1d5e20b80ea2416674343206344f3ed3
CaTxDog <57478694+CaTxDog@users.noreply.github.com>
2021-07-06 17:59:11 +0600

    ДЗ №2.1-№2.3
    До преобразования функции в метод

--- sandbox/src/main/java/ru/truakdsg/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/truakdsg/pft/sandbox/MFP.java
@@ -2,7 +2,13 @@ package ru.truakdsg.pft.sandbox;
 
 public class MFP {
 
+  public static double distance(Point p1, Point p2)
+  {
+    return Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
+  }
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    Point p1 = new Point(1,2);
+    Point p2 = new Point(3,4);
+    System.out.println(distance(p1,p2));
   }
 }
