a32d6e78201e4e6b6f1082aae0d5df5f2583f0b6
CaTxDog <57478694+CaTxDog@users.noreply.github.com>
2021-07-09 17:49:48 +0600

    Настройка среды для testng

--- sandbox/src/main/java/ru/truakdsg/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/truakdsg/pft/sandbox/MFP.java
@@ -7,5 +7,6 @@ public class MFP {
     Point p1 = new Point(1,2);
     Point p2 = new Point(3,4);
     System.out.println("Расстояние между точками P1("+p1.x+","+p1.y+") и P2("+p2.x+","+p2.y+") = " + p1.distance(p2));
+    System.out.println(p2.distance(p1));
   }
 }
