a706a9f3d1db2d58304f48a7fcc92ba98abe7410
CaTxDog <57478694+CaTxDog@users.noreply.github.com>
2021-07-13 15:06:08 +0600

    Полный рефакторинг + удаление группы

--- addressbook-web-tests/src/test/java/ru/truakdsg/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/truakdsg/pft/addressbook/ContactCreationTest.java
@@ -1,85 +1,14 @@
 package ru.truakdsg.pft.addressbook;
 
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactCreationTest {
-  private WebDriver wd;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
-
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    login("admin", "secret");
-  }
-
-  private void login(String login, String password) {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(login);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+public class ContactCreationTest extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    findLinkText("add new");
+    gotoContactPage();
     fillContactForm(new ContactData("Sergey", "Ivanov", "Terminator228", "Raif", "Omsk", "79998887766", "78889995566", "test@test1.com", "test1@test1.com"));
-    findLinkText("home page");
-  }
-
-  private void findLinkText(String s) {
-    wd.findElement(By.linkText(s)).click();
-  }
-
-  private void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhone());
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
+    returnHomePage();
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/truakdsg/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/truakdsg/pft/addressbook/GroupCreationTest.java
@@ -1,82 +1,17 @@
 package ru.truakdsg.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupCreationTest {
-  private WebDriver wd;
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    login("admin", "secret");
-  }
-
-  private void login(String login, String password) {
-    wd.get("http://localhost/addressbook/");
-    clickByName("user");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(login);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+public class GroupCreationTest extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-    findLinkText("groups");
-    clickByName("new");
-    fillGroupForm(new GroupData("test1", "test2", "test3"));
-    clickByName("submit");
-    findLinkText("group page");
-  }
-
-  private void clickByName(String s) {
-    wd.findElement(By.name(s)).click();
-  }
-
-  private void fillGroupForm(GroupData groupData) {
-    clickByName("group_name");
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    clickByName("group_header");
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    clickByName("group_footer");
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void findLinkText(String groups) {
-    wd.findElement(By.linkText(groups)).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test5", "test5", "test5"));
+    submitGroupCreation();
+    returnGroupPage();
   }
 
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    findLinkText("Logout");
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-  
 }
